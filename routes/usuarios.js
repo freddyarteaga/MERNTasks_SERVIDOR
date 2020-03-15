7621d672d1b68df9db7e4ae201651ff14b9d099b
Freddy <freddy.artea@gmail.com>
2020-03-15 16:47:53 -0500

    configurando rutas de usuarios

---
+++ routes/usuarios.js
@@ -0,0 +1,10 @@
+// Rutas para Crear Usuarios
+const express = require('express')
+const router = express.Router()
+
+// Crear un usuario
+// api/usuarios
+router.post('/', () => {
+
+})
+module.exports = router;
