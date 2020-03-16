e18c75670fa97bdb21f7df5bf0a50bd7fa51eeed
Freddy <freddy.artea@gmail.com>
2020-03-16 18:20:21 -0500

    revisando si un proyecto existe o no

--- controllers/proyectoController.js
+++ controllers/proyectoController.js
@@ -50,18 +50,29 @@ exports.actualizarProyecto = async (req, res) => {
         const nuevoProyecto = {}
 
         if ( nombre ) {
-            nuevoProyecto = nombre
+            nuevoProyecto.nombre = nombre
         }
 
         try {
 
             // revisar el ID
+            let proyecto = await Proyecto.findById(req.params.id)
 
             // si el proyecto existe o no
+            if (!proyecto) {
+                return res.status(400).json({ msg: 'Proyecto no encontrado' })
+            }
 
             // verficar el creador del proyecto
+            if (proyecto.creador.toString() !== req.usuario.id) {
+                return res.status(401).json({ msg: 'No Autorizado' })
+            }
 
             // actualizar
+            proyecto = await Proyecto.findByIdAndUpdate({ _id: req.params.id }, { $set : 
+            nuevoProyecto }, { new: true })
+
+            res.json({proyecto})
             
         } catch (error) {
             console.log(error)
