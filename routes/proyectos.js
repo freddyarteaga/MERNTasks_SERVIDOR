9aeb60191eaba9491dc84e6aed670b627542791f
Freddy <freddy.artea@gmail.com>
2020-03-15 19:00:12 -0500

    agregando routing y controllers para proyectos

---
+++ routes/proyectos.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router()
+const proyectoController = require('../controllers/proyectoController')
+// Crea proyectos
+// api/proyectos
+router.post('/',
+    proyectoController.crearProyecto
+)
+
+module.exports = router;
